80f4ca690674719d572078f54adadca871f3e3ef
nift4 <nift4@protonmail.com>
2022-08-20 16:08:00 +0200

    update

--- nicloud.sh
+++ nicloud.sh
@@ -1,18 +1,17 @@
 function write_page {
-	write_header "NiCloud" "nift4" "NiCloud is just the fancy name for my small server &amp; domain farm." "https://nift4.org/nicloud.html"
+	write_header "NiCloud" "nift4" "NiCloud is just the fancy name for my small server farm." "https://nift4.org/nicloud.html"
 	cat <<EOF
-    NiCloud is just the fancy name for my small server &amp; domain farm.<br/>
+    NiCloud is just the fancy name for my small server farm.<br/>
     It consists of:
     <ul>
-       <li>o1 - Oracle Cloud Free Plan provided to me by <a href="https://alex-dev.me">Alex</a>!</li>
-       <li>o1-vps1 - Oracle Cloud Free x86_64 vps with 1GB RAM</li>
-       <li>o1-vps2 - Oracle Cloud Free x86_64 vps with 1GB RAM</li>
-       <li>o1-vps3 - Oracle Cloud Free arm64 vps with 24GB RAM</li>
-       <li>d1 - The domain "nift4.org" provided to me by <a href="https://alex-dev.me">Alex</a>!</li>
-       <li>d2 - The domain "nift4.eu.org" provided to me by <a href="https://nic.eu.org">nic.eu.org</a>!</li>
-       <li>d3 - The domain "droid-ng.eu.org" provided to me by <a href="https://nic.eu.org">nic.eu.org</a>!</li>
+       <li>micro1 - Oracle Cloud Free x86_64 vps with 1GB RAM (Alex)</li>
+       <li>micro2 - Oracle Cloud Free x86_64 vps with 1GB RAM (Alex)</li>
+       <li>big - Oracle Cloud Free arm64 vps with 24GB RAM (Alex)</li>
+       <li>micro3 - Oracle Cloud Free x86_64 vps with 1GB RAM (Jonas)</li>
+       <li>micro4 - Oracle Cloud Free x86_64 vps with 1GB RAM (Jonas)</li>
+       <li>sirius - Oracle Cloud Free arm64 vps with 24GB RAM (Lambada)</li>
     </ul>
-    Additionally mention-worthy is that <a href="https://t.me/kiam001">kiam001</a> is providing me an android build server, thank you :D
+    Thanks to <a href="https://alex-dev.me">Alex</a>, Lambada and Jonas!
 EOF
 	write_footer
 }
